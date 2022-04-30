a14858b59a38fef3f19b27502a599701c2446a2f
brohan <35300101+nathanbro16@users.noreply.github.com>
2022-04-30 18:01:28 +0200

    Create registration

---
+++ app/Models/Registrations.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Registrations extends Model
+{
+    use HasFactory;
+
+    /**
+    * The attributes that are mass assignable.
+    *
+    * @var array<int, string>
+    */
+    protected $fillable = [
+        'Title',
+        'IsSuccess',
+        'IsOpen',
+        'DateOpen',
+        'DateClosed',
+        'CheckList'
+
+    ];
+
+    public function Families() {
+        return $this->hasMany('MembersFamilies', 'registrations_id');
+    }
+}
---
+++ database/migrations/2022_04_20_132052_create_registrations_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('registrations', function (Blueprint $table) {
+            $table->id();
+            $table->string('Title', 20);
+            $table->string('IsSuccess', 250);
+            $table->boolean('IsOpen');
+            $table->date('DateOpen');
+            $table->date('DateClosed');
+            $table->json('CheckList');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('registrations');
+    }
+};
