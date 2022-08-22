fc603ca38426ecd8457e7a232bd804c1b6b7cd9b
brohan <35300101+nathanbro16@users.noreply.github.com>
2022-08-22 16:46:24 +0200

    Update App.jsx

--- resources/js/App.jsx
+++ resources/js/App.jsx
@@ -1,17 +1,25 @@
 import '#css/App.css';
-import { Routes, Route } from 'react-router-dom';
-import Dashboard from '@/pages/dashboard/index';
+import { Routes, Route, useParams } from 'react-router-dom';
 import Login from '@/pages/login';
 import Register from '@/pages/register';
 import Home from '@/pages/home';
 import ForgotPassword from '@/pages/forgot-password';
 import PasswordReset from '@/pages/password-reset';
 import NotFoundPage from '@/pages/404';
-import RegisterLicense from '@/pages/dashboard/register';
+
 import AppLayout from '@/components/Layouts/AppLayout';
+
+import Dashboard from '@/pages/dashboard/index';
+import RegisterLicense from '@/pages/dashboard/register/index';
+import FamiliesList from '@/pages/dashboard/register/Families';
+
+import Inscriptions from '@/pages/form/inscriptions';
+import ListForm from '@/pages/form/index';
+
 import { QueryClient, QueryClientProvider } from 'react-query';
 
 const queryClient = new QueryClient();
+
 
 function App() {
   return (
@@ -25,11 +33,17 @@ function App() {
         <Route path="/password-reset/:token" element={<PasswordReset />} />
         <Route path='/dashboard' element={<AppLayout/>} >
           <Route index element={<Dashboard />} />
-          <Route path="register" element={<RegisterLicense />} />
-
+          <Route path='register'>
+            <Route index element={<RegisterLicense />} />
+            <Route path=':RegisterId' element={<FamiliesList/>}  />
+            <Route path='edit/:RegisterId' element={<FamiliesList/>}  />
+          </Route>
         </Route>
-        <Route path="*" element={<NotFoundPage/>}
-        />
+        <Route path='/form'>
+          <Route index element={ <ListForm/> } />
+          <Route path='inscr/:form' element={<Inscriptions />}/>
+        </Route>
+        <Route path="*" element={<NotFoundPage/>}/>
       </Routes>
     </div>
     </QueryClientProvider>
