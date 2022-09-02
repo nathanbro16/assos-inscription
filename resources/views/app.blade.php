748ce45e477caf154ffa157f28631bc5abc396e5
brohan <35300101+nathanbro16@users.noreply.github.com>
2022-09-02 21:24:57 +0200

    add google fonts

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,7 +7,7 @@
         <title >{{ config('app.name', 'Laravel') }}</title>
 
         <!-- Fonts -->
-
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
         <!-- Styles -->
         <link href="{{ mix('css/index.css') }}" rel="stylesheet">
         <!-- Scripts -->
