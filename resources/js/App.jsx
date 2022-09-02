25ee776a4e011cc9676308c7bfb11c3d739c38af
brohan <35300101+nathanbro16@users.noreply.github.com>
2022-09-02 21:24:35 +0200

    Add snackbar for notifications

--- resources/js/App.jsx
+++ resources/js/App.jsx
@@ -1,4 +1,3 @@
-import '#css/App.css';
 import { Routes, Route, useParams } from 'react-router-dom';
 import Login from '@/pages/login';
 import Register from '@/pages/register';
@@ -16,7 +15,14 @@ import FamiliesList from '@/pages/dashboard/register/Families';
 import Inscriptions from '@/pages/form/inscriptions';
 import ListForm from '@/pages/form/index';
 
-import { QueryClient, QueryClientProvider } from 'react-query';
+import { StyledEngineProvider } from '@mui/material/styles';
+
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+import { SnackbarProvider } from 'notistack';
+
+import Footer from './components/Layouts/footer';
 
 const queryClient = new QueryClient();
 
@@ -24,28 +30,35 @@ const queryClient = new QueryClient();
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-    <div className="antialiased">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/forgot-password" element={<ForgotPassword />} />
-        <Route path="/password-reset/:token" element={<PasswordReset />} />
-        <Route path='/dashboard' element={<AppLayout/>} >
-          <Route index element={<Dashboard />} />
-          <Route path='register'>
-            <Route index element={<RegisterLicense />} />
-            <Route path=':RegisterId' element={<FamiliesList/>}  />
-            <Route path='edit/:RegisterId' element={<FamiliesList/>}  />
-          </Route>
-        </Route>
-        <Route path='/form'>
-          <Route index element={ <ListForm/> } />
-          <Route path='inscr/:form' element={<Inscriptions />}/>
-        </Route>
-        <Route path="*" element={<NotFoundPage/>}/>
-      </Routes>
-    </div>
+      <StyledEngineProvider injectFirst>
+        <SnackbarProvider maxSnack={3} autoHideDuration={2000}>
+
+        <div className="antialiased">
+          <div className='content' >
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/forgot-password" element={<ForgotPassword />} />
+            <Route path="/password-reset/:token" element={<PasswordReset />} />
+            <Route path='/dashboard' element={<AppLayout/>} >
+              <Route index element={<Dashboard />} />
+              <Route path='register'>
+                <Route index element={<RegisterLicense />} />
+                <Route path=':RegisterId' element={<FamiliesList/>}  />
+                <Route path='edit/:RegisterId' element={<FamiliesList/>}  />
+              </Route>
+            </Route>
+            <Route path='/form'>
+              <Route index element={ <ListForm/> } />
+              <Route path='inscr/:form' element={<Inscriptions />}/>
+            </Route>
+            <Route path="*" element={<NotFoundPage/>}/>
+          </Routes>
+          </div>
+        </div>
+        </SnackbarProvider>
+      </StyledEngineProvider>
     </QueryClientProvider>
   );
 }
